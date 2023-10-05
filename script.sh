85cbf3f55a59aecd998086f7a870acddfd17cba1
mohamed_drayef <amindrayef@gmail.com>
2023-10-05 11:25:03 +0200

    Correction exercice

--- script.sh
+++ script.sh
@@ -1,1 +1,7 @@
-tree
+
+
+mkdir -p public/{css/{footer,header,main},img,js}
+touch index.html public/css/footer/style.css
+touch public/css/header/style.css
+touch public/css/main/style.css
+touch public/img/img1 public/img/img2
